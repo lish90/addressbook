63c38634ae598921cfa5098d58e161e58c7f5da3
alisha musamabik <alisha.musamabik@gmail.com>
2020-05-24 12:53:39 +0100

    start project -understanding useffect hooks

--- src/App.js
+++ src/App.js
@@ -1,48 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-const App = () => { 
-  return(
-    <div>
-    <ContactCard/>
-    <ContactCard/>
-    <ContactCard/>
-    <Card/>
-  </div>
-    
-);
-}
+const App = () => {
+  const [contacts, setContacts] = useState([]);
 
-const ContactCard = () => {
-  // 1st variable access the state value, 2nd enables changes.
-  const [showAge, setShowAge] = useState(true);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+      .then(response => response.json())
+      .then(data => {
+        setContacts(data.results);
+      });
+  }, []);
+
+  return (
+    <>
+      {contacts.map(contact => (
+        <ContactCard
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
+    </>
+  );
+};
+
+
+
+
+  // const contacts = [
+  //   { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25, star_sign:"Traus"},
+  //   { name: "Jason Long", email: "jason.long@notreal.com", age: 45, star_sign:"Traus"},
+  //   { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100, star_sign:"pear" },
+  //   { name: "Amy McDonald", email: "amy@email.com", age: 33, star_sign:"cancner" }
+  // ];
+  
+    // <>
+    //   <ContactCard
+    //     avatar="https://via.placeholder.com/150"
+    //     name="Jenny Han"
+    //     email="jenny.han@notreal.com"
+    //     age={25}
+    //   />
+    //   <ContactCard
+    //     avatar="https://via.placeholder.com/150"
+    //     name="Jason Long"
+    //     email="jason.long@notreal.com"
+    //     age={45}
+    //   />
+    //   <ContactCard
+    //     avatar="https://via.placeholder.com/150"
+    //     name="Peter Pan"
+    //     email="peter.pan@neverland.com"
+    //     age={100}
+    //   />
+    // </>
+//   );
+// };
+
+const ContactCard = props => {
+  const [showAge, setShowAge] = useState(false);
   
 
   return (
     <div className="contact-card">
-      <img src="https://via.placeholder.com/150" alt="profile" />
+      <img src={props.avatar} alt="profile" />
       <div className="user-details">
-        <p>Name: Jenny Han</p>
-        <p>Email: Jenny.Han@notreal.com</p>
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
         <button onClick={() => setShowAge(!showAge)}>
-	Toggle Age 
-</button>
-        {showAge === true ? <p>Age: 25</p> : null}
+			    Toggle Age 
+		     </button>
+        {showAge && <p>Age: {props.age}</p>}
       </div>
     </div>
   );
 };
 
- const Card=()=>{
-   return(
-     <div>
-       <h2> Tasty burger</h2>
-       {/* <img>https://source.unsplash.com/400x300/?burger</img> */}
-       <p>It's good burger</p>
-     </div>
+//  const Card=()=>{
+//    return(
+//      <div>
+//        <h2> Tasty burger</h2>
+//        {/* <img>https://source.unsplash.com/400x300/?burger</img> */}
+//        <p>It's good burger</p>
+//      </div>
 
-   );
- }
+//    );
+//  }
  
 
 
